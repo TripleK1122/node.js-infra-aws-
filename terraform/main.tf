200669315b312de2f5a6fe1fee3bb06012ad1381
TripleK1122 <kostyaone0@gmail.com>
2025-11-01 17:06:38 -0500

    changed infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,3 +101,72 @@ resource "aws_lb_listener" "webapp" {
     target_group_arn = aws_lb_target_group.webapp.arn
   }
 }
+
+# --- ECS Cluster ---
+resource "aws_ecs_cluster" "webapp" {
+  name = "webapp-cluster"
+}
+
+# --- IAM Role for ECS Task Execution ---
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole-webapp"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_attach" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# --- ECS Task Definition ---
+resource "aws_ecs_task_definition" "webapp" {
+  family                   = "webapp-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name      = "webapp"
+      image     = "${aws_ecr_repository.webapp.repository_url}:latest"
+      essential = true
+      portMappings = [{
+        containerPort = 3000
+        protocol      = "tcp"
+      }]
+    }
+  ])
+}
+
+# --- ECS Service ---
+resource "aws_ecs_service" "webapp" {
+  name            = "webapp-service"
+  cluster         = aws_ecs_cluster.webapp.id
+  task_definition = aws_ecs_task_definition.webapp.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = module.vpc.public_subnets
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.webapp.arn
+    container_name   = "webapp"
+    container_port   = 3000
+  }
+
+  depends_on = [aws_lb_listener.webapp]
+}
